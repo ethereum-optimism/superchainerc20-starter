7a37454975d3380b61fc47bc901b6445e6a98e6c
tre <tremarkley@gmail.com>
2024-10-15 16:07:30 -0700

    fix

--- contracts/script/SingleChainSuperchainERC20Deployment.s.sol
+++ contracts/script/SingleChainSuperchainERC20Deployment.s.sol
@@ -27,7 +27,6 @@ contract SingleChainSuperchainERC20Deployment is Script {
         string memory symbol = vm.envString("ERC20_SYMBOL");
         uint256 decimals = vm.envOr("ERC20_DECIMALS", uint256(18));
         bytes memory erc20InitCode = type(ExampleERC20).creationCode;
-        // addr_ = address(new ExampleERC20{salt: implSalt()}({ _owner: owner, _name: name, _symbol: symbol, _decimals: decimals }));
         addr_ = ICreateX(Preinstalls.CreateX).deployCreate3({
             salt: implSalt(),
             initCode: abi.encodePacked(erc20InitCode, abi.encode(owner, name, symbol, decimals))
@@ -35,12 +34,13 @@ contract SingleChainSuperchainERC20Deployment is Script {
         console.log("Deployed L2NativeSuperchainERC20 at address: ", addr_);
     }
 
-    /// @notice The CREATE2 salt to be used when deploying the token.
+    /// @notice The CREATE3 salt to be used when deploying the token.
     function implSalt() internal view returns (bytes32) {
         string memory salt = vm.envOr("SALT", string("ethers phoenix"));
         bytes32 encodedSalt = keccak256(abi.encodePacked(salt));
         return bytes32(
-            abi.encodePacked(msg.sender, hex"01", bytes11(encodedSalt))
+            // constructed in order to provide re-deploy protection: https://github.com/pcaversaccio/createx/blob/058bc3b07e082711457d8ea20d8767a37a5a0021/src/CreateX.sol#L922
+            abi.encodePacked(bytes20(msg.sender), hex"01", bytes11(encodedSalt))
         );
     }
 }
