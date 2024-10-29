260172b504fe450e031b5598032d5c40fedd46dd
tre <tremarkley@gmail.com>
2024-10-29 16:15:36 -0700

    feat: integrate superchain registry rpc urls (#20)

--- packages/contracts/scripts/MultiChainSuperchainERC20Deployment.s.sol
+++ packages/contracts/scripts/MultiChainSuperchainERC20Deployment.s.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.25;
 
 import {console} from "forge-std/Script.sol";
 import {Vm} from "forge-std/Vm.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {stdToml} from "forge-std/StdToml.sol";
 import {Script} from "forge-std/Script.sol";
 import {ICreateX} from "createx/ICreateX.sol";
 import {Preinstalls} from "@contracts-bedrock/libraries/Preinstalls.sol";
@@ -13,13 +15,14 @@ contract MultiChainSuperchainERC20Deployment is Script, SuperchainERC20Deployer 
     function setUp() public {}
 
     function run() public {
-        string[2][] memory rpcUrls = vm.rpcUrls();
-        for (uint256 i = 0; i < rpcUrls.length; i++) {
-            string memory rpcAlias = rpcUrls[i][0];
+        string[] memory chainsToDeployTo = vm.parseTomlStringArray(deployConfig, ".multi_chain_deploy_config.chains");
 
-            console.log("Deploying to chain: ", rpcAlias);
+        for (uint256 i = 0; i < chainsToDeployTo.length; i++) {
+            string memory chainToDeployTo = chainsToDeployTo[i];
 
-            vm.createSelectFork(rpcAlias);
+            console.log("Deploying to chain: ", chainToDeployTo);
+
+            vm.createSelectFork(chainToDeployTo);
             deployL2NativeSuperchainERC20();
         }
     }
