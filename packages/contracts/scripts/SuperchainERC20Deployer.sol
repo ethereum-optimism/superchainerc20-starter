42427f57da6c493a97ff271ed5e983fa51f3d2c4
tre <tremarkley@gmail.com>
2024-11-01 15:11:59 -0700

    feat: use built in create2 (#35)

--- packages/contracts/scripts/SuperchainERC20Deployer.sol
+++ packages/contracts/scripts/SuperchainERC20Deployer.sol
@@ -28,39 +28,24 @@ contract SuperchainERC20Deployer {
         string memory name = vm.parseTomlString(deployConfig, ".token.name");
         string memory symbol = vm.parseTomlString(deployConfig, ".token.symbol");
         uint256 decimals = vm.parseTomlUint(deployConfig, ".token.decimals");
-        addr_ = _deployCreate2({
-            _creationCode: type(L2NativeSuperchainERC20).creationCode,
-            _constructorParams: abi.encode(owner, name, symbol, decimals)
-        });
-    }
-
-    /// @notice The CREATE2 salt to be used when deploying the token.
-    function _implSalt() internal view returns (bytes32) {
-        string memory salt = vm.parseTomlString(deployConfig, ".deploy_config.salt");
-        return keccak256(abi.encodePacked(salt));
-    }
-
-    /// @notice Deploys a contract using the CREATE2 opcode.
-    /// @param _creationCode The contract creation code.
-    /// @param _constructorParams The constructor parameters.
-    function _deployCreate2(bytes memory _creationCode, bytes memory _constructorParams)
-        internal
-        returns (address addr_)
-    {
-        bytes32 salt = _implSalt();
-        bytes memory initCode = abi.encodePacked(_creationCode, _constructorParams);
-        address preComputedAddress = vm.computeCreate2Address(salt, keccak256(initCode));
+        require(decimals <= type(uint8).max, "decimals exceeds uint8 range");
+        bytes memory initCode =
+            abi.encodePacked(type(L2NativeSuperchainERC20).creationCode, abi.encode(owner, name, symbol, uint8(decimals)));
+        address preComputedAddress = vm.computeCreate2Address(_implSalt(), keccak256(initCode));
         if (preComputedAddress.code.length > 0) {
             console.log(
                 "L2NativeSuperchainERC20 already deployed at %s", preComputedAddress, "on chain id: ", block.chainid
             );
             addr_ = preComputedAddress;
         } else {
-            assembly {
-                addr_ := create2(0, add(initCode, 0x20), mload(initCode), salt)
-            }
-            require(addr_ != address(0), "L2NativeSuperchainERC20 deployment failed");
+            addr_ = address(new L2NativeSuperchainERC20{salt: _implSalt()}(owner, name, symbol, uint8(decimals)));
             console.log("Deployed L2NativeSuperchainERC20 at address: ", addr_, "on chain id: ", block.chainid);
         }
+    }
+
+    /// @notice The CREATE2 salt to be used when deploying the token.
+    function _implSalt() internal view returns (bytes32) {
+        string memory salt = vm.parseTomlString(deployConfig, ".deploy_config.salt");
+        return keccak256(abi.encodePacked(salt));
     }
 }
