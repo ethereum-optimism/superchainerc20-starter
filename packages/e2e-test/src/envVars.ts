c4bffa580cd1234dc95751af77be150a89543ff6
tre <tremarkley@gmail.com>
2024-11-05 14:53:01 -0800

    feat: remove createx (#47)

--- packages/e2e-test/src/envVars.ts
+++ packages/e2e-test/src/envVars.ts
@@ -2,6 +2,6 @@ import { parseEnv, z } from 'znv'
 import { Address as ZodAddress } from 'abitype/zod'
 
 export const envVars = parseEnv(import.meta.env, {
-  VITE_TOKEN_CONTRACT_ADDRESS: ZodAddress.default("0xe20d8C366AE0716ca88384feCbcbf8179581157D"),
+  VITE_TOKEN_CONTRACT_ADDRESS: ZodAddress.default("0x040fC8E3cb47245BaAE006286e8EC177Ff676C1C"),
   VITE_TOKEN_MINTER_ADDRESS: ZodAddress.default("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
 })
