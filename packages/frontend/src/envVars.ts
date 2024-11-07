e3524c2f781dc26dad896cad802faf813c60e681
tre <tremarkley@gmail.com>
2024-11-07 12:23:10 -0800

    feat: update envVars to handle empty strings (#53)

--- packages/frontend/src/envVars.ts
+++ packages/frontend/src/envVars.ts
@@ -3,7 +3,12 @@ import { Address as ZodAddress } from 'abitype/zod'
 import { deployment } from '@superchainerc20-starter/contracts'
 
 export const envVars = parseEnv(import.meta.env, {
-  VITE_TOKEN_CONTRACT_ADDRESS: ZodAddress.default(deployment.deployedAddress),
-  VITE_TOKEN_MINTER_ADDRESS: ZodAddress.default(deployment.ownerAddress),
+  VITE_TOKEN_CONTRACT_ADDRESS: z
+    .string()
+    .default(deployment.deployedAddress)
+    .transform((val) => {
+      const address = val.trim() !== '' ? val : deployment.deployedAddress
+      return ZodAddress.parse(address)
+    }),
   VITE_WALLET_CONNECT_PROJECT_ID: z.string().optional(),
 })
