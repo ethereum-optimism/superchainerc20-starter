ff9712dd35af52898e50d463fb69cc6ca17b8737
James Kim <jakim929@gmail.com>
2024-11-07 15:09:08 -0800

    fix: use impersonateAccount to drip

--- packages/e2e-test/src/envVars.ts
+++ packages/e2e-test/src/envVars.ts
@@ -1,20 +1,18 @@
 import { parseEnv, z } from 'znv'
 import { Address as ZodAddress } from 'abitype/zod'
 import { deployment } from '@superchainerc20-starter/contracts'
+import { Address } from 'viem'
+
+const zAddressWithDefault = (defaultAddress: Address) =>
+  z
+    .string()
+    .default(defaultAddress)
+    .transform((val) => {
+      const address = val.trim() !== '' ? val : defaultAddress
+      return ZodAddress.parse(address)
+    })
 
 export const envVars = parseEnv(import.meta.env, {
-  VITE_TOKEN_CONTRACT_ADDRESS: z
-    .string()
-    .default(deployment.deployedAddress)
-    .transform((val) => {
-      const address = val.trim() !== '' ? val : deployment.deployedAddress
-      return ZodAddress.parse(address)
-    }),
-  VITE_TOKEN_MINTER_ADDRESS: z
-    .string()
-    .default(deployment.ownerAddress)
-    .transform((val) => {
-      const address = val.trim() !== '' ? val : deployment.ownerAddress
-      return ZodAddress.parse(address)
-    }),
+  VITE_TOKEN_CONTRACT_ADDRESS: zAddressWithDefault(deployment.deployedAddress),
+  VITE_TOKEN_MINTER_ADDRESS: zAddressWithDefault(deployment.ownerAddress),
 })
--- packages/frontend/src/envVars.ts
+++ packages/frontend/src/envVars.ts
@@ -1,14 +1,19 @@
 import { parseEnv, z } from 'znv'
 import { Address as ZodAddress } from 'abitype/zod'
 import { deployment } from '@superchainerc20-starter/contracts'
+import { Address } from 'viem'
+
+const zAddressWithDefault = (defaultAddress: Address) =>
+  z
+    .string()
+    .default(defaultAddress)
+    .transform((val) => {
+      const address = val.trim() !== '' ? val : defaultAddress
+      return ZodAddress.parse(address)
+    })
 
 export const envVars = parseEnv(import.meta.env, {
-  VITE_TOKEN_CONTRACT_ADDRESS: z
-    .string()
-    .default(deployment.deployedAddress)
-    .transform((val) => {
-      const address = val.trim() !== '' ? val : deployment.deployedAddress
-      return ZodAddress.parse(address)
-    }),
+  VITE_TOKEN_CONTRACT_ADDRESS: zAddressWithDefault(deployment.deployedAddress),
+  VITE_TOKEN_MINTER_ADDRESS: zAddressWithDefault(deployment.ownerAddress),
   VITE_WALLET_CONNECT_PROJECT_ID: z.string().optional(),
 })
