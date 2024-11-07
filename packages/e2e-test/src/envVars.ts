aa47e95841f31b96b0baed2572b8c82c35ed6c1e
tre <tremarkley@gmail.com>
2024-11-07 11:54:24 -0800

    feat: update envVars to handle empty strings

--- packages/e2e-test/src/envVars.ts
+++ packages/e2e-test/src/envVars.ts
@@ -3,6 +3,18 @@ import { Address as ZodAddress } from 'abitype/zod'
 import { deployment } from '@superchainerc20-starter/contracts'
 
 export const envVars = parseEnv(import.meta.env, {
-  VITE_TOKEN_CONTRACT_ADDRESS: ZodAddress.default(deployment.deployedAddress),
-  VITE_TOKEN_MINTER_ADDRESS: ZodAddress.default(deployment.ownerAddress),
+  VITE_TOKEN_CONTRACT_ADDRESS: z
+    .string()
+    .default(deployment.deployedAddress)
+    .transform((val) => {
+      const address = val.trim() !== '' ? val : deployment.deployedAddress
+      return ZodAddress.parse(address)
+    }),
+  VITE_TOKEN_MINTER_ADDRESS: z
+    .string()
+    .default(deployment.ownerAddress)
+    .transform((val) => {
+      const address = val.trim() !== '' ? val : deployment.ownerAddress
+      return ZodAddress.parse(address)
+    }),
 })
